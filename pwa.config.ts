c47e11da4f946c9701569df533fea3dd882198e5
Syed Faiez Bin Hafeez <114837076+faiezhafeez@users.noreply.github.com>
2026-09-21 04:18:26 +0500

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -11,16 +11,4 @@ export default defineConfig({
   orientation: "portrait",
   start_url: "/",
   scope: "/",
-  shortcuts: [
-    {
-      name: "AIOU Assignments",
-      short_name: "Assignments",
-      url: "/p/aiou-assignments.html",
-    },
-    {
-      name: "Guess Papers",
-      short_name: "Guess",
-      url: "/p/aiou-guess-paper.html",
-    },
-  ],
 });
